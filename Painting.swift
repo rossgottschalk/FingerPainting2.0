414c60b7aadcf9da8b281a7dbf5bf8f25aba858d
Ross Gottschalk <rossgottschalk@yahoo.com>
2016-08-26 15:51:10 -0400

    Core Data Functions. Now working on Deleting core data

---
+++ Painting.swift
@@ -0,0 +1,17 @@
+//
+//  Painting.swift
+//  FingerPainting2.0
+//
+//  Created by Ross Gottschalk on 8/26/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Painting: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
